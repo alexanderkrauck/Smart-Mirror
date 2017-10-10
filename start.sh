0653b7106c9c20141bcf0cb06d52f72f0639831b
Krauck1 <alexander.krauck@gmail.com>
2017-10-10 19:00:51 +0200

    Improved the sh file

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 cd HTML;
 python -m SimpleHTTPServer 8000 &
 sleep 1;
-google-chrome --kiosk http://localhost:8000/index.html
+
+if [ "$#" = "0" ]
+then
+chromium-browser --noerrdialogs --kiosk http://localhost:8000/index.html;
+elif [ "$1" = "-u" ]
+then
+google-chrome --noerrdialogs --kiosk http://localhost:8000/index.html;
+elif [ "$1" = "-t" ]
+then
+google-chrome http://localhost:8000/index.html;
+fi
